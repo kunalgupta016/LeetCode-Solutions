e675ce3f601fc04b99794aea97d8b6a906c66fda
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-03-25 17:26:42 +0530

    Time: 0 ms (100%), Space: 8.1 MB (79.74%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(isupper(word[0]) && isupper(word[1])){
+            for(int i = 2;i<word.size();i++){
+                if(islower(word[i])) return false;
+            }
+        }
+        else if(isupper(word[0]) && !isupper(word[1])){
+            for(int i = 2;i<word.size();i++){
+                if(isupper(word[i])) return false;
+            }
+        }
+        else{
+            for(int i = 0;i<word.size();i++){
+                if(isupper(word[i])) return false;
+            }
+        }
+        return true;
+    }
+};
