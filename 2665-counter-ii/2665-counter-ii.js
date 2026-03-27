e73cd745525b573e4bc340a17ca66647c0c7f9aa
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-03-27 23:44:10 +0530

    Time: 63 ms (5.48%), Space: 56.8 MB (39.17%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -1,27 +1,9 @@
-/**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
- */
 var createCounter = function(init) {
     let cnt = init;
-    function increment(){
-        return ++cnt;
-    }
-    function decrement(){
-        return --cnt;
-    }
-    function reset(){
 
-        return (cnt=init);
-    }
-    return {increment,decrement,reset};
-
-
+    return {
+        increment: () => ++cnt,
+        decrement: () => --cnt,
+        reset: () => (cnt = init)
+    };
 };
-
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
