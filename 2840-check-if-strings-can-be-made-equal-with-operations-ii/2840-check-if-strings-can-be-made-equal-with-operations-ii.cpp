d71c23a9a64cb7f6fda045261d826c215f1e0689
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-03-29 11:06:03 +0530

    Time: 34 ms (34.67%), Space: 20.7 MB (48%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        unordered_map<char,int> even;
+        unordered_map<char,int> odd;
+        for(int i = 0;i<s1.size();i++){
+            if(i&1){
+                odd[s1[i]]++;
+            }else{
+                even[s1[i]]++;
+            }
+        }
+        for(int i = 0;i<s2.size();i++){
+            if(i&1){
+                odd[s2[i]]--;
+                if(odd[s2[i]]==0){
+                    odd.erase(s2[i]);
+                }
+            }else{
+                even[s2[i]]--;
+                if(even[s2[i]]==0){
+                    even.erase(s2[i]);
+                }
+            }
+        }
+        return( odd.size()==0 && even.size()==0);
+    }
+};
