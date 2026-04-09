5e1b41a6f7238b66e745dc9170cfce08d406450d
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-04-09 23:59:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
@@ -4,14 +4,14 @@ public:
         sort(arr.begin(),arr.end());
         int n = arr.size();
         int x = 0.05*n;
-        double mean  = 0.00000;
-        int sum = 0;
+        
+        double sum = 0;
 
         for(int i = x;i<n-x;i++){
             sum += arr[i];
         }
-        mean = (double)sum/(n-2*x);
-        return mean;
+        return sum/(n-2*x);
+ 
 
     }
 };
