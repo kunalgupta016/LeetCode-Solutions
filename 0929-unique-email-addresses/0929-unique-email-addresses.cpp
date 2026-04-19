442557ffe059cfb62f5a13833eaf953cb7fefbda
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-04-19 23:59:41 +0530

    Time: 4 ms (76.59%), Space: 18.8 MB (28.35%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+
+        set<string> st;
+
+        for(string email : emails){
+
+            string local = "";
+            string domain = "";
+
+            int i=0;
+
+            while(email[i] != '@'){
+
+                if(email[i] == '+'){
+                    while(email[i] != '@'){
+                        i++;
+                    }
+                    break;
+                }
+
+                if(email[i] != '.'){
+                    local += email[i];
+                }
+
+                i++;
+            }
+
+            i++; 
+            while(i < email.size()){
+                domain += email[i];
+                i++;
+            }
+
+            st.insert(local + "@" + domain);
+        }
+
+        return st.size();
+    }
+};
