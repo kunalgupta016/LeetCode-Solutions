6e7507ebc44c5a4d0bbf2af3551639a92abd2633
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-05-08 20:56:39 +0530

    Time: 647 ms (62.36%), Space: 371.2 MB (34.32%) - LeetHub

---
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+
+    vector<int> isPrime;
+    void build(int maxEle){
+        isPrime.resize(maxEle+1,true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for(int i = 2;i*i<=maxEle;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=maxEle;j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+    }
+
+    int minJumps(vector<int>& nums) {
+        int n = nums.size();
+        queue<int> q;
+        q.push(0);
+        unordered_map<int,vector<int>> mp;
+        int maxEle = 0;
+        for(int i = 0;i<n;i++){
+            mp[nums[i]].push_back(i);
+            maxEle = max(maxEle,nums[i]);
+        }
+
+        build(maxEle);
+        int steps = 0;
+        vector<bool> visited(n,false);
+        visited[0] = true;
+        unordered_set<int> seen;
+        seen.insert(0);
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                int i = q.front();
+                q.pop();
+                if(i == n-1) return steps;
+                if(i-1>=0 && !visited[i-1]){
+                    visited[i - 1] = true;
+                    q.push(i - 1);
+                }
+                if(i+1<=n-1 && !visited[i+1]){
+                    visited[i + 1] = true;
+                    q.push(i + 1);
+                }
+
+                if(!isPrime[nums[i]]|| seen.count(nums[i])){
+                    continue;
+                }
+                for(int j = nums[i];j<=maxEle;j+=nums[i]){
+                    if(!mp.contains(j)){
+                        continue;
+                    }
+                    for(auto it:mp[j]){
+                        if(!visited[it]){
+                            q.push(it);
+                            visited[it] = true;
+                        }
+                    }
+                }
+                seen.insert(nums[i]);
+
+            }
+            steps++;
+        }
+        return steps;
+    }
+}; 
