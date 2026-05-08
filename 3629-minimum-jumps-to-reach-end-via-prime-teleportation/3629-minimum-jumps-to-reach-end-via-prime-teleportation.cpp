50526a6de3a08be82a1f07e610712c8b951a307c
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-05-08 21:18:50 +0530

    Time: 647 ms (62.36%), Space: 376.5 MB (30.63%) - LeetHub

--- 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -8,29 +8,31 @@ public:
         isPrime[1] = false;
         for(int i = 2;i*i<=maxEle;i++){
             if(isPrime[i]){
-                for(int j=i*i;j<=maxEle;j+=i){
+                for(int j = i*i;j<=maxEle;j+=i){
                     isPrime[j] = false;
                 }
-            }
+            }   
         }
-
     }
+
 
     int minJumps(vector<int>& nums) {
         int n = nums.size();
-        queue<int> q;
-        q.push(0);
         unordered_map<int,vector<int>> mp;
         int maxEle = 0;
         for(int i = 0;i<n;i++){
             mp[nums[i]].push_back(i);
+            
             maxEle = max(maxEle,nums[i]);
         }
 
         build(maxEle);
-        int steps = 0;
-        vector<bool> visited(n,false);
+        queue<int> q;
+        q.push(0);
+        vector<int> visited(n,false);
         visited[0] = true;
+        int steps = 0;
+
         unordered_set<int> seen;
         seen.insert(0);
         while(!q.empty()){
@@ -38,35 +40,33 @@ public:
             while(size--){
                 int i = q.front();
                 q.pop();
-                if(i == n-1) return steps;
+
+                if(i==n-1) return steps;
                 if(i-1>=0 && !visited[i-1]){
-                    visited[i - 1] = true;
-                    q.push(i - 1);
+                    q.push(i-1);
+                    visited[i-1] = true;
                 }
                 if(i+1<=n-1 && !visited[i+1]){
-                    visited[i + 1] = true;
-                    q.push(i + 1);
+                    q.push(i+1);
+                    visited[i+1] = true;
                 }
+                if(!isPrime[nums[i]] || seen.count(nums[i])) continue;
 
-                if(!isPrime[nums[i]]|| seen.count(nums[i])){
-                    continue;
-                }
-                for(int j = nums[i];j<=maxEle;j+=nums[i]){
-                    if(!mp.contains(j)){
-                        continue;
-                    }
-                    for(auto it:mp[j]){
-                        if(!visited[it]){
-                            q.push(it);
-                            visited[it] = true;
+
+                for(int mul = nums[i];mul<=maxEle;mul+=nums[i]){
+                    if(!mp.count(mul)) continue;
+                    for(auto idx:mp[mul]){
+                        if(!visited[idx]){
+                            q.push(idx);
+                            visited[idx] = true;
                         }
                     }
                 }
                 seen.insert(nums[i]);
-
             }
             steps++;
         }
         return steps;
+
     }
 }; 
