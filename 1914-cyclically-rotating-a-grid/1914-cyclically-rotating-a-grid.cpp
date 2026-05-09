b37162162b037e2fa48f056590cfd8cdf07dbcdb
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-05-09 14:45:16 +0530

    Time: 16 ms (45.59%), Space: 18.2 MB (23.53%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -1,46 +1,76 @@
 class Solution {
 public:
+
     vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
-        int t = 0;
-        int l = 0;
-        int b = grid.size()-1;
-        int r = grid[0].size()-1;
 
-        while(t<b && l<r){
+        int m = grid.size();
+        int n = grid[0].size();
 
-            int len = b-t;
-            int wid = r-l;
-            int p = 2*len + 2*wid;
-            int j = k%p;
-            while(j--){
+        int top = 0;
+        int left = 0;
+        int bottom = m - 1;
+        int right = n - 1;
 
-                int temp = grid[t][l];
+        while (top < bottom && left < right) {
 
+            vector<int> layer;
 
-                for(int i = l;i<r;i++){
-                    grid[t][i] = grid[t][i+1];
-                }
-
-                for(int i = t;i<b;i++){
-                    grid[i][r] = grid[i+1][r];
-                }
-
-                for(int i = r;i>l;i--){
-                    grid[b][i] = grid[b][i-1];
-                }
-
-                for(int i = b;i>t;i--){
-                    grid[i][l] = grid[i-1][l];
-                }
-                grid[t+1][l] = temp;
-
+            
+            for (int i = left; i <= right; i++) {
+                layer.push_back(grid[top][i]);
             }
-            t++;
-            l++;
-            b--;
-            r--;
 
+            
+            for (int i = top + 1; i <= bottom - 1; i++) {
+                layer.push_back(grid[i][right]);
+            }
+
+            
+            for (int i = right; i >= left; i--) {
+                layer.push_back(grid[bottom][i]);
+            }
+
+            
+            for (int i = bottom - 1; i >= top + 1; i--) {
+                layer.push_back(grid[i][left]);
+            }
+
+            int sz = layer.size();
+
+            vector<int> rotated(sz);
+
+            for (int i = 0; i < sz; i++) {
+                rotated[i] = layer[(i + k) % sz];
+            }
+
+            int idx = 0;
+
+            
+            for (int i = left; i <= right; i++) {
+                grid[top][i] = rotated[idx++];
+            }
+
+            
+            for (int i = top + 1; i <= bottom - 1; i++) {
+                grid[i][right] = rotated[idx++];
+            }
+
+            
+            for (int i = right; i >= left; i--) {
+                grid[bottom][i] = rotated[idx++];
+            }
+
+            
+            for (int i = bottom - 1; i >= top + 1; i--) {
+                grid[i][left] = rotated[idx++];
+            }
+
+            top++;
+            left++;
+            bottom--;
+            right--;
         }
+
         return grid;
     }
 };
