5972d47e4e6c0109635ff2c38a6c50426959ef22
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-05-25 10:09:25 +0530

    Time: 30 ms (51.55%), Space: 52 MB (43.53%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& alice, vector<int>& bob) {
+        int sumA = 0;
+        int sumB = 0;
+        for(int a:alice) sumA+=a;
+        for(int b:bob) sumB+=b;
+
+        int diff = (sumB-sumA)/2;
+        unordered_set<int>setB(bob.begin(),bob.end());
+        for(int a:alice){
+            if(setB.count(a+diff)){
+                return {a,a+diff};
+                
+            }
+        }
+        return {};   
+
+    }
+};
