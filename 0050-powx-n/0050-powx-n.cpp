359debabbcfcec7108039b207558d3105d9fe9f8
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-06-03 12:49:19 +0530

    Time: 0 ms (100%), Space: 8.6 MB (70.1%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long exp = n;
+        double res = 1.0;
+        if(exp<0){
+            x = 1/x;
+            exp = -exp;
+        }
+        while(exp>0){
+            if(exp%2!=0){
+                res = res*x;
+            }
+            x = x*x;
+            exp = exp>>1;
+        }
+        return res;
+    }
+};
