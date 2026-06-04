1ae9a825cd97b852a780d0475a4298212d664ef5
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-06-04 13:57:40 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.24%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -7,7 +7,6 @@ public:
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k--] = nums1[i--];
-
             }else{
                 nums1[k--] = nums2[j--];
             }
@@ -16,6 +15,5 @@ public:
         while(j>=0){
             nums1[k--] = nums2[j--];
         }
-        
     }
 };
