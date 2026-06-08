89d328901c3124b20b130fcde00e59510dbba77e
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-06-08 10:56:41 +0530

    Time: 0 ms (100%), Space: 133.5 MB (53.14%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> small;
-        vector<int> large;
-        map<int,int> mp;
-        for(int i:nums){
-            mp[i]++;
+        vector<int>ans ;
+        for(auto x : nums)
+        {
+            if(x < pivot)
+            ans.push_back(x);
         }
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i]<pivot){
-                small.push_back(nums[i]);
-            }else if(nums[i]>pivot){
-                large.push_back(nums[i]);
-            }
+        for(auto x : nums)
+        {
+            if(x == pivot)
+            ans.push_back(x);
         }
-        vector<int> ans;
-        for(int i:small){
-            ans.push_back(i);
-        }
-
-        int freq = mp[pivot];
-        for(int i = 0;i<freq;i++){
-            ans.push_back(pivot);
-        }
-
-        for(int i:large){
-            ans.push_back(i);
+        for(auto x : nums)
+        {
+            if(x >pivot)
+            ans.push_back(x);
         }
         return ans;
     }
