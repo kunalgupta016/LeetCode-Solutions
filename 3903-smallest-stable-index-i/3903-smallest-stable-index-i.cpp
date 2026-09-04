1f86a60356abf2a07a3a2012f0c6164e800d2794
Kunal Gupta <140793986+kunalgupta016@users.noreply.github.com>
2026-09-04 12:56:37 +0530

    Time: 0 ms (100%), Space: 30.3 MB (85.58%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> maxi(n);
-        maxi[0] = nums[0];
-        vector<int> mini(n);
-        mini[n-1] = nums[n-1];
-        int maxEle = nums[0];
-        for(int i = 1;i<n;i++){
-            maxEle = max(nums[i],maxEle);
-            maxi[i] = maxEle;
+        int mini = INT_MAX;
+        for(int i = 0;i<nums.size();i++){
+            int maxEle = *max_element(nums.begin(),nums.begin()+i);
+            int minEle = *min_element(nums.begin()+i,nums.end());
+            int instability = maxEle - minEle;
+            if(instability<=k){
+                mini = min(mini,i);
+            }
         }
-        int minEle = nums[n-1];
-        for(int i = n-2;i>=0;i--){
-            minEle = min(nums[i],minEle);
-            mini[i] = minEle;
+        if(mini==INT_MAX){
+            return -1;
+        }else{
+            return mini;
         }
-        vector<int> ans(n);
-        for(int i = 0;i<n;i++){
-            int diff = maxi[i]-mini[i];
-            if(diff<=k) return i;
-
-        }
-        return -1;
-
-
     }
 };
